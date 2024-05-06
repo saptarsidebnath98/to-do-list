d416136cbe074484707da2f7808346ff8acefc77
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-05-07 00:34:51 +0530

    webpack installation

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+    ],
+};
